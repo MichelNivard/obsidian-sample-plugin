2caf5fe9b149bd60c75c4f03425af41805aacddf
Michel Nivard <m.g.nivard@gmail.com>
2024-02-05 15:12:16 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -74,64 +74,7 @@ export default class MyPlugin extends Plugin {
 
 
 
-/// HEre is where I began copy pasting things:
-
-
-
-extract(src, lineNumber, variants = DEFAULT.variants) {
-
-    function is(line, target) {
-        let str = line.trim()
-        return str.toUpperCase() === target.toUpperCase();
-    }
-
-    let lines = src.split('\n')
-    let begin = null
-    let end = null
-    let lang = null
-
-    function fenceOpeningWithKey(line) {
-        for (var key of Object.keys(variants)) {
-            if (is(line, '```' + key)) {
-                return key
-            }
-        }
-        return null
-    }
-
-
-    for (let i = lineNumber; i >= 0; i--) {
-
-        let key = fenceOpeningWithKey(lines[i])
-        if (key) {
-            begin = i;
-            lang = key
-            break
-        } else if (i !== lineNumber && is(lines[i], '```')) {
-            begin = null
-            lang = null
-            break
-        }
-    }
-
-    for (let i = lineNumber; i < lines.length; i++) {
-        if (i !== begin && is(lines[i], '```')) {
-            end = i;
-            break
-        }
-    }
-
-    if ((begin != null) && (end != null)) {
-        return {
-            lang: lang,
-            text: lines.slice(begin + 1, end).join('\n'),
-            begin: begin,
-            end: end,
-        };
-    }
-    return null
-
-}
+/// Here is where I began copy pasting things:
 
 runSnippet() {
         let vars = this.get_vars();
